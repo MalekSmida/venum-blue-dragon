4d4ae0ad6a59f510d5d84578621657e068ac9721
Malek Smida <msmida96@gmail.com>
2022-11-25 11:03:44 +0100

    Feat: extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './sections/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        445: '27.813rem',
+      },
+      height: {
+        665: '41.563rem',
+      },
+      maxWidth: {
+        1920: '1920px',
+      },
+    },
   },
   plugins: [],
 };
