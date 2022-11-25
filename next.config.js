83f3ca5a105d4cbd9285f525150a6c71bde7a790
Malek Smida <msmida96@gmail.com>
2022-11-25 11:02:57 +0100

    Feat: use optimized next images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.shopify.com', 'a.storyblok.com', 'euro.venum.com'],
+  },
 };
 
 module.exports = nextConfig;
