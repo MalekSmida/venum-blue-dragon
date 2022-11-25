1ce842486611abded2b690447efb7ddb37f38364
Malek Smida <msmida96@gmail.com>
2022-11-25 11:06:11 +0100

    #Feat: add mocks, fake data

---
+++ sections/Collections/mocks.ts
@@ -0,0 +1,40 @@
+import { Collection } from '../../types';
+
+export const data: Array<Collection> = [
+  {
+    _id: '0',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+  {
+    _id: '1',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+  {
+    _id: '2',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+  {
+    _id: '3',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+  {
+    _id: '4',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+  {
+    _id: '5',
+    image: 'https://euro.venum.com/media/wysiwyg/PUSH-RAZOR-HOMME.jpg',
+    title: 'Ufc fight night 2.0',
+    link: '',
+  },
+];
