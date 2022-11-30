f463b09cfc57535e5cf965247582b68bf4dd0e69
Malek Smida <msmida96@gmail.com>
2022-11-30 20:43:18 +0100

    #Refactor: mocks

--- sections/Custom/mocks.ts
+++ sections/Custom/mocks.ts
@@ -42,7 +42,7 @@ export const dataCustom: Array<ICollection> = [
     imageDesk:
       'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802130/Venum/Groupe_de_masques_129_vrdzyv.jpg',
     imageMobile:
-      'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802299/Venum/Mobile/Gants-Custom_Mobile_ne1ltu.jpg',
+      'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802301/Venum/Mobile/Kits-Personnalise%CC%81-Custom_Mobile_btxqee.jpg',
     title: 'Kits personnalisés',
     description:
       "8 modèles de gants, jusqu'à 20 zones personnalisables, près de 20 couleurs, 3 matières… Des possibilités infinies pour créer des gants qui te ressemblent.",
@@ -60,7 +60,7 @@ export const dataCustom: Array<ICollection> = [
     imageDesk:
       'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802129/Venum/Groupe_de_masques_128_b0jalm.jpg',
     imageMobile:
-      'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802299/Venum/Mobile/Gants-Custom_Mobile_ne1ltu.jpg',
+      'https://res.cloudinary.com/deebb8zh7/image/upload/v1669802301/Venum/Mobile/Kits-Fighter-Custom_Mobile_qif2fb.jpg',
     title: 'Kits fighters',
     description:
       "8 modèles de gants, jusqu'à 20 zones personnalisables, près de 20 couleurs, 3 matières… Des possibilités infinies pour créer des gants qui te ressemblent.",
